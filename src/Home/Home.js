643f2b58dba72cc3f202406154eca918ce835e09
Hoangphohieu <hoangphohieu1@gmail.com>
2020-08-29 17:51:50 +0700

    hieu dz

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -11,15 +11,23 @@ function Home(props) {
 
         // https://drive.google.com/drive/folders/1A9SjJ_R4g1JYf6MTW7krUeEUKgRmHQJB/?usp=sharing
 
-        const result = await axios({
+        let result = await axios({
             method: 'post',
             url: constants.NodeURL,
             data: {
                 link: id,
-              
+
             }
         });
-    console.log(result);
+        result = result.data
+
+        if (result.length === 0) alert("kiem tra lai url")
+        else {
+            result = result.filter(item => item.mimeType === "application/vnd.google-apps.folder").map(item=>{return {name:item.name,id:item.id}});
+
+        }
+        console.log(result);
+
 
     }
     // let result = LinkParent.split("/").pop();
