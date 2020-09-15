23205765b59789fc2e54b91207b159c86ad1fd12
Hoangphohieu <hoangphohieu1@gmail.com>
2020-09-15 18:40:18 +0700

    hieu

---
+++ front/precache-manifest.e76c3c7975e674ac5f3fc69a1f884b66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f7083ed23c316b9d3726d080fe1fe47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "848481d1d9d13982cd24",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "988cc4c8f2c8b0819dae",
+    "url": "/static/js/2.a8147ff5.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.a8147ff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "848481d1d9d13982cd24",
+    "url": "/static/js/main.57b2267a.chunk.js"
+  },
+  {
+    "revision": "73b432106fb81810a66a",
+    "url": "/static/js/runtime-main.09d06ce2.js"
+  }
+]);
